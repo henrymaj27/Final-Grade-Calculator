cfca66a2b05a05565441facff9f947a95016389b
Henry Majewski <henryjmajewski@icloud.com>
2021-07-26 12:01:36 -0400

    Initial Commit

---
+++ Final Grade Calculator/Final_Grade_CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Final_Grade_CalculatorApp.swift
+//  Final Grade Calculator
+//
+//  Created by Henry Majewski on 7/21/21.
+//
+
+import SwiftUI
+
+@main
+struct Final_Grade_CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
